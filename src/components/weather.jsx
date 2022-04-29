470476e8b1c2891d0e3e52585b508a6a5d407fcd
holmista <mishadaro21@gmail.com>
2022-04-30 01:15:54 +0400

    feat: add convert button and style it

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -5,21 +5,26 @@ import WeatherCard from './weatherCard';
 
 export default function Weather() {
   const { lat, lon } = useParams();
-  const [measure, setMeasure] = useState('celcius');
+  const [measure, setMeasure] = useState('metric');
 
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${process.env.REACT_APP_KEY2}`;
+  const handleMeasure = () => {
+    if (measure === 'metric') setMeasure('imperial');
+    else setMeasure('metric');
+  };
+
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=${measure}&appid=${process.env.REACT_APP_KEY2}`;
   const { data, loading, error } = useFetch(url);
   return (
-    <div className="flex flex-col items-center justify-ceneter min-h-screen bg-slate-500 overflow-auto py-16 sm:py-0 sm:h-screen">
+    <div className="flex flex-col items-center justify-ceneter min-h-screen  overflow-auto  sm:h-screen">
 
-      <div className="bg-white w-screen flex justify-end pr-44 pt-5">
-        <button type="button">
-          {`convert to ${measure}`}
+      <div className="w-screen flex justify-center sm:flex sm:justify-end sm:pr-48">
+        <button onClick={handleMeasure} type="button" className="p-1 shrink-0 mt-10 bg-indigo-300 w-40 h-14 rounded-lg">
+          {`convert to ${measure === 'metric' ? 'imperial' : 'metric'}`}
         </button>
       </div>
 
-      <div className="h-full flex justify-center items-center bg-red-400">
-        <div className="grid grid-cols-1 xl:grid-cols-4 sm:grid-cols-2">
+      <div className="h-full flex justify-center items-center py-16 sm:py-0">
+        <div className="grid grid-cols-1 xl:grid-cols-4 sm:grid-cols-2 ">
           {data && data.daily.map((elem) => (
             <WeatherCard
               key={elem.dt}
